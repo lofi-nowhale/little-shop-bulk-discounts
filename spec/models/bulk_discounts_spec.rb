e40518f9082a4a806dc0fa3aac0d3a3b00166a9e
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-09-22 11:21:38 -0600

    test: write tests for the bulk_discount model

---
+++ spec/models/bulk_discounts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe BulkDiscounts, type: :model do 
+  describe "relationships" do 
+    it { should belong_to :merchant } 
+  end
+
+  describe "validations" do 
+    it { should validate_numericality_of :percentage }
+    it { should validate_numericality_of :threshold }
+  end
+
+end
