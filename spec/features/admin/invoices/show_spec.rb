57938c54f4bb66d63bfbd3800a59f40deb74eb0a
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-09-26 23:55:10 -0600

    test: admin invoice discounted revenue and setup the show page to reflect

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -64,7 +64,7 @@ describe "Admin Invoices Index Page" do
   it "should display the total discounted revenue the invoice will generate" do
     expect(page).to have_content("Discounted Revenue: $#{@i1.discounted_revenue}")
 
-    expect(page).to_not have_content(@i2.discounted_revenue)
+    expect(page).to_not have_content(@i2.total_revenue)
   end
 
   it "should have status as a select field that updates the invoices status" do
